c82b9a7b9f7b2c69ec91acf9d6ed3640ebe17bc1
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-02-04 01:23:43 -0300

    Add Exercicio02.java for summing two numbers

---
+++ Exercicio02.java
@@ -0,0 +1,11 @@
+public class Exercicio02 {
+    //SOMA DE DOIS NÚMEROS
+    public static void main(String[] args){
+        int a = 10;
+        int b = 5;
+        int soma = a + b;
+        //"texto" + variável
+
+        System.out.println("A soma é: " + soma);
+    }
+}
