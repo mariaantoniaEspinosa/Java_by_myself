0fe16d9b1342841e657f34e73cfe8099c5e2e514
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-02-04 01:26:06 -0300

    Add Exercicio08 class with a for loop example

---
+++ Exercicio08.java
@@ -0,0 +1,8 @@
+public class Exercicio08 {
+    //USO DO FOR
+    public static void main(String[]args){
+        for(int i = 1; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+}
