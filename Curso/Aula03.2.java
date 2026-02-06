8dfcc3d2e33fe59f9552f174d3172fd8ec7a90ba
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-02-06 14:39:36 -0300

    Add Alunotestar class to demonstrate Aluno usage

---
+++ Curso/Aula03.2.java
@@ -0,0 +1,14 @@
+public class Alunotestar {
+    public static void main(String[] args){
+        Aluno aluno1 = new Aluno();
+
+        //aluno1.nome = "Maria";
+        aluno1.setNome("Maria");
+
+        System.out.println(aluno1.getNome());
+
+        //aluno1.nome = 500;
+
+        aluno1.setIdade(50);
+    }
+}
