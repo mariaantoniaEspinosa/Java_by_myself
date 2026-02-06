f9cd44f05a9d1490ab5d47a1a920a4c2e7c6dd2d
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-02-06 14:46:52 -0300

    Delete Exercicio08.java

--- Exercicio08.java
+++
@@ -1,8 +0,0 @@
-public class Exercicio08 {
-    //USO DO FOR
-    public static void main(String[]args){
-        for(int i = 1; i<=10; i++){
-            System.out.println(i);
-        }
-    }
-}
