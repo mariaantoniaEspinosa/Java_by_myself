917b7bbe495a89eafcdf0612a78e1df0c0b06662
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-02-06 20:48:31 -0300

    Create 05Polimorfismo.1.java

---
+++ ExercíciosGPTpt2/05Polimorfismo.1.java
@@ -0,0 +1,50 @@
+public class Aluno {
+
+    //atributo privado
+    private String nome;
+    private double nota1;
+    private double nota2;
+
+    protected double getNota1(){
+        return nota1;
+    }
+    protected double getNota2(){
+        return nota2;
+    }
+
+    //construtor da classe aluno
+    Aluno(String nome, double nota1, double nota2){
+        this.nome = nome;
+        //this = atributo
+        // = nome = parametro
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+    }
+
+    public Aluno() {
+    }
+
+    //Getter: acesso ao nome de forma controlada
+    public String getNome(){
+        return nome;
+    }
+
+    //Setter: altera o nome com controle
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    //metodo que calcula a media
+    double media(){
+        return (nota1 + nota2)/2;
+    }
+
+    //metodo da decisao
+    String situacao(){
+        if(media()>=7){
+            return "Aprovado";
+        }else {
+            return "Reprovado";
+        }
+    }
+}
