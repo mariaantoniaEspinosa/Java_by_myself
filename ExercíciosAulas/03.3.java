813fc0c2e1d6efbb82b6ad9abcd703b1f4eacfd2
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-02-21 21:11:06 -0300

    Create 03.3.java

---
+++ ExercíciosAulas/03.3.java
@@ -0,0 +1,8 @@
+public class laços03 {
+    public static void main(String[]args){
+        int i;
+        for(i=1; i<=10; i++){
+            System.out.println("7 * " + i + " = " + (7*i));
+        }
+    }
+}
